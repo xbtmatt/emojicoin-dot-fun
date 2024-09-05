5cc9a7d259e8b30e4e0ce14b09db0a4fdabad625
matt <xbtmatt@gmail.com>
2024-09-04 22:10:44 -0700

    Fix entrypoint commands

--- src/docker/test-deployer/sh/entrypoint.sh
+++ src/docker/test-deployer/sh/entrypoint.sh
@@ -14,32 +14,36 @@ fund_amount=20000000000000000
 extra_for_gas=200000000
 gas_unit_price=100
 
-faucet_url="http://host.docker.internal:8081"
-rest_url="http://host.docker.internal:8080/v1"
+profile="publisher"
+
+aptos init \
+	--assume-no \
+	--profile $profile \
+	--rest-url http://host.docker.internal:8081 \
+	--faucet-url http://host.docker.internal:8080/v1 \
+	--private-key $publisher_private_key \
+	--encoding hex \
+	--network custom
 
 aptos account fund-with-faucet \
 	--faucet-url $faucet_url \
-	--account $publisher_address \
+	--account $profile \
 	--amount $((fund_amount + extra_for_gas))
 
 # Publish with the JSON publish payloads generated in the build step.
 aptos move run \
-	--json-file /app/json/emojicoin_dot_fun.json \
 	--assume-yes \
+	--json-file /app/json/emojicoin_dot_fun.json \
 	--max-gas 2000000 \
 	--gas-unit-price $gas_unit_price \
-	--url $rest_url \
-	--private-key $publisher_private_key \
-	--encoding hex
+	--profile $profile
 
 aptos move run \
-	--json-file /app/json/rewards.json \
 	--assume-yes \
+	--json-file /app/json/rewards.json \
 	--max-gas 2000000 \
 	--gas-unit-price $gas_unit_price \
-	--url $rest_url \
-	--private-key $publisher_private_key \
-	--encoding hex
+	--profile $profile
 
 # ------------------------------------------------------------------------------
 #                              Fund the test accounts
@@ -54,12 +58,8 @@ for i in {1..2}; do
 	batch_fund_output_path="${batch_fund_path_prefix}-${i}.json"
 	aptos move run \
 		--assume-yes \
-		--private-key $publisher_private_key \
-		--encoding hex \
 		--json-file $batch_fund_output_path \
 		--max-gas $max_gas \
 		--gas-unit-price 100 \
-		--url $rest_url \
-		--private-key $publisher_private_key \
-		--encoding hex
+		--profile $profile
 done
